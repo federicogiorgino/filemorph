5fa843e8495fa403f63b946f1df272fd32ba249f
Federico Giorgino <fed.giorgino@outlook.com>
2024-09-25 15:30:54 +0200

    feat: header + theme provider and toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Header } from "@/components/header";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +20,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div>{children}</div>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          themes={["light", "dark"]}
+        >
+          <Header />
+          <div>{children}</div>
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
